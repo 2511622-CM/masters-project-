0e4b7f1964eb2be47a2a6f888c826d329167a171
2511622-CM <2511622@uad.ac.uk>
2026-08-02 15:19:59 +0100

    Update home.php

--- home.php
+++ home.php
@@ -44,7 +44,7 @@ include ('components.php');
 </header>
 
 <body>
-  <h2 class="h2">Efficient Online Shopping - This is an automated update</h2>
+  <h2 class="h2">Efficient Online Shopping - This is an automated update v2</h2>
 
   <!--Card Layout -->
   <main class="container my-4">
