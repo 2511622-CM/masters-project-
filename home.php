81c423cd55d66c607644e8b4eb80621a499bd952
2511622-CM <2511622@uad.ac.uk>
2026-08-03 10:34:48 +0100

    Update home.php

--- home.php
+++ home.php
@@ -44,7 +44,7 @@ include ('components.php');
 </header>
 
 <body>
-  <h1 class="h1">Efficient Online Shopping CHANGE V2</h1>
+  <h1 class="h1">Efficient Online Shopping</h1>
 
   <!--Card Layout -->
   <main class="container my-4">
