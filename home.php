d5d00f2b1eaa5c949733f1634320367bedf939b9
CM <2511622@uad.ac.uk>
2026-08-07 20:47:18 +0100

    Update 6 - s2

--- home.php
+++ home.php
@@ -44,7 +44,7 @@ include ('components.php');
 </header>
 
 <body>
-  <h1 class="h1">Efficient Online Shopping - Scenario 2 - testing number five</h1>
+  <h1 class="h1">Efficient Online Shopping - Scenario 2 - testing number 6</h1>
 
   <!--Card Layout -->
   <main class="container my-4">
