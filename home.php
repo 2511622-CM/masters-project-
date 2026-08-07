1d377543b304547f3f1722b5f1eabfa1ed8aa08c
CM <2511622@uad.ac.uk>
2026-08-07 20:34:29 +0100

    Update 1.5

--- home.php
+++ home.php
@@ -44,7 +44,7 @@ include ('components.php');
 </header>
 
 <body>
-  <h1 class="h1">Efficient Online Shopping - s2 update 1 testing</h1>
+  <h1 class="h1">Efficient Online Shopping - s2 update 1.5 testing</h1>
 
   <!--Card Layout -->
   <main class="container my-4">
