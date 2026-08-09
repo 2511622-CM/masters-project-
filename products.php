c70d4d27a54695a4d9787bb385071e875df166cd
CM <2511622@uad.ac.uk>
2026-08-09 17:28:35 +0100

    s3 test 7

--- products.php
+++ products.php
@@ -6,7 +6,7 @@ Products
 -->
 <?php
 include ('configv3.php');
-include ('components.php');
+include ('components.php')
 
 $category_name = trim($_GET['category'] ?? '');
 $product_name  = trim($_GET['product_name'] ?? '');
@@ -24,7 +24,7 @@ $result = $stmt->get_result();
 $product = $result->fetch_assoc();
 
 $image_text = urlencode($product['product_name']);
-image_sorc = "https://placehold.co/300x300/EEE/31343C?font=pt-sans&text=" . $image_text;
+$image_src = "https://placehold.co/300x300/EEE/31343C?font=pt-sans&text=" . $image_text;
 
 if (!$product) {
     header("HTTP/1.0 404 not found");
