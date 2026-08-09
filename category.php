828d2005302b248c803e704440295e84f417fdf6
CM <2511622@uad.ac.uk>
2026-08-09 15:53:40 +0100

    S3 test 1

--- category.php
+++ category.php
@@ -16,9 +16,9 @@ if (empty($category_name)) {
 }
 
 $stmt = $conn->prepare("SELECT * FROM products WHERE LOWER(category) = LOWER(?)");
-$stmt->bind_param("s", $category_name);
-$stmt->execute();
-$result = $stmt->get_result(); ?>
+$stmt->bind_param("s", $category_name)
+$stmt->execute()
+$result = $stmt->get_result() ?>
 
 <!DOCTYPE html>
 <html lang="en">
