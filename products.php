05aa2d8b85b49c28f237a2a111fc73386395fb59
CM <2511622@uad.ac.uk>
2026-08-09 16:01:37 +0100

    revert

--- products.php
+++ products.php
@@ -21,7 +21,7 @@ $stmt->bind_param("ss", $product_name, $category_name);
 $stmt->execute();
 
 $result = $stmt->get_result();
-$productssssss = $result->fetch_assoc();
+$product = $result->fetch_assoc();
 
 $image_text = urlencode($product['product_name']);
 $image_src = "https://placehold.co/300x300/EEE/31343C?font=pt-sans&text=" . $image_text;
