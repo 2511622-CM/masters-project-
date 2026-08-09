edc79ebe35d4f1133a1c9baa2c19d88c0be539f8
CM <2511622@uad.ac.uk>
2026-08-09 17:29:39 +0100

    s3 test 8

--- products.php
+++ products.php
@@ -6,7 +6,7 @@ Products
 -->
 <?php
 include ('configv3.php');
-include ('components.php')
+include ('components.php');
 
 $category_name = trim($_GET['category'] ?? '');
 $product_name  = trim($_GET['product_name'] ?? '');
@@ -21,7 +21,7 @@ $stmt->bind_param("ss", $product_name, $category_name);
 $stmt->execute();
 
 $result = $stmt->get_result();
-$product = $result->fetch_assoc();
+proooduct = $result->fetch_assoc();
 
 $image_text = urlencode($product['product_name']);
 $image_src = "https://placehold.co/300x300/EEE/31343C?font=pt-sans&text=" . $image_text;
